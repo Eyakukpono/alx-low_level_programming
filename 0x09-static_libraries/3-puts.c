655fad34b49b9b2c66d5e1fcd4c8a3cd2e38a9b7
Eyakukpono <eyakieanne99@gmail.com>
2023-04-14 12:30:22 -0700

    update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,6 +1,17 @@
+#include <stdio.h>
 #include "main.h"
+
 void _puts(char *str)
-#include<stdio.h>
+{
+int i = 0;
+while (str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+putchar('\n');
+}
+
 /**
 * main - Entry point
 *
@@ -8,6 +19,6 @@ void _puts(char *str)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+_puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
